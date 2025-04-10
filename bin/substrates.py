f4b922e26a4ad141e1656b675799c5602471ddab
ASundus <48042931+ASundus@users.noreply.github.com>
2025-04-10 15:46:07 -0700

    end

--- bin/substrates.py
+++ bin/substrates.py
@@ -1769,12 +1769,9 @@ class SubstrateTab(object):
             # print('plot_svg with frame=',self.svg_frame)
             self.plot_svg(self.svg_frame)
             # cbar = self.fig.colorbar(substrate_plot, ax=self.ax0)
-            self.png_frame += 1 
-            png_file = os.path.join(self.output_dir, f"frame{self.png_frame:04d}.png") 
-            self.fig.savefig(png_file)
+   
 
-
-       # if (self.analysis_data_toggle.value):
+        if (self.analysis_data_toggle.value):
         # if (self.substrate_frame > 0):  # rwh: when to plot extra analysis (custom data)?
             # print('analysis_data_toggle.value =',self.analysis_data_toggle.value )
             # self.plot_analysis_data("time", ["assembled_virion"], -1)
@@ -1782,8 +1779,11 @@ class SubstrateTab(object):
             self.substrate_frame = int(frame / self.modulo)
             # self.plot_analysis_data("time", ["assembled_virion"], self.substrate_frame)
             self.plot_analysis_data(self.substrate_frame)
-      #  else:
-       #     self.plot_empty_analysis_data()
+        else:
+            self.plot_empty_analysis_data()
 
         if self.colab_flag:
             plt.show()
+    self.png_frame += 1 
+    png_file = os.path.join(self.output_dir, f"frame{self.png_frame:04d}.png") 
+    self.fig.savefig(png_file)
